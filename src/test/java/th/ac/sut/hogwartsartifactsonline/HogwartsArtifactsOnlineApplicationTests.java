a96a7c6060b7e7bfa4087322861c4c78bcd5092f
pimsupa <pim.saengsupawat@gmail.com>
2023-05-11 14:15:10 +0700

    This is the first commit.

---
+++ src/test/java/th/ac/sut/hogwartsartifactsonline/HogwartsArtifactsOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package th.ac.sut.hogwartsartifactsonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsArtifactsOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
